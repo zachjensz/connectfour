8e4ef61da0bef5615506f357904958f166c0a8c5
Zach Jensz <zach@zachjensz.com>
2022-12-09 19:29:53 +1100

    socketio working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,14 @@
 <script>
   import Grid from '$lib/Grid.svelte'
   import Banner from '$lib/Banner.svelte'
+
+  import { io } from 'socket.io-client'
+
+  const socket = io()
+
+  socket.on('eventFromServer', (message) => {
+    console.log(message)
+  })
 </script>
 
 <Banner />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
+import { Server } from 'socket.io'
+
+const webSocketServer = {
+	name: 'webSocketServer',
+	configureServer(server) {
+		const io = new Server(server.httpServer)
+
+		io.on('connection', (socket) => {
+			socket.emit('eventFromServer', 'Hellhaohgh')
+		})
+	},
+}
+
+
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		webSocketServer,
+	]
 };
 
 export default config;
