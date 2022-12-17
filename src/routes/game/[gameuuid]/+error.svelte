0c038b6b0d5b46af7b5213edf7a1461aae400550
Zach Jensz <zach@zachjensz.com>
2022-12-17 12:32:16 +1100

    Reworded 404 page

--- src/routes/game/[gameuuid]/+error.svelte
+++ src/routes/game/[gameuuid]/+error.svelte
@@ -3,7 +3,7 @@
 </script>
 
 {#if $page.status == "404"}
-<h1>Game empty (404)</h1>
+<h1>Game Unavailable (404)</h1>
 <p><em>{$page.error.message}</em> isn't around anymore, either
   <a href="/host">host a new game</a>
   or
