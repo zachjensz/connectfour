1b63a40bcb6ad399df10b1d5f6154dbbb6fc39af
Zach Jensz <zach@zachjensz.com>
2023-07-15 01:31:39 +1000

    unfinished abomination

--- client/src/lib/Slot.svelte
+++ client/src/lib/Slot.svelte
@@ -1,78 +1,117 @@
 <script>
-	export let slot = 0
+	export let slot = 0;
 	export let hover = false;
 	export let drophint = false;
 	export let dance = false;
+
+	function fillColour(slot) {
+		switch (slot) {
+			case 0:
+				return 'none';
+			case 1:
+				return 'var(--c-discSecondary)';
+			case 2:
+				return 'var(--c-discPrimary)';
+		}
+	}
+	function strokeColour(slot) {
+		switch (slot) {
+			case 0:
+				return 'none';
+			case 1:
+				return 'var(--c-discSecondaryOutline)';
+			case 2:
+				return 'var(--c-discPrimaryOutline)';
+		}
+	}
+	const SLOT_SIZE = 40; // 0-50
+	const SLOT_OUTLINE = 4;
+	const DISC_SIZE = 40; // 0-50
+	const DISC_OUTLINE = 4;
 </script>
 
 <div
 	class="slot"
-	class:hover={hover}
-	class:drophint={drophint}
-	class:dance={dance}
-	class:primary={slot === 1}
-	class:secondary={slot === 2}
+	class:hover
+	class:drophint
+	class:dance
 	on:click
 	on:mouseover
 	on:mouseleave
-/>
+>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 100 100"
+		width="100"
+		height="100"
+	>
+		<mask id="circle-clip">
+			<rect width="100" height="100" fill="white" />
+			<circle cx="50" cy="50" r={SLOT_SIZE} fill="black" />
+		</mask>
+		<rect
+			width="100"
+			height="100"
+			mask="url(#circle-clip)"
+			fill="var(--c-board)"
+		/>
+		<circle
+			cx="50"
+			cy="50"
+			r={SLOT_SIZE - SLOT_OUTLINE / 2}
+			fill="none"
+			stroke="var(--c-board-outline)"
+			stroke-width={SLOT_OUTLINE}
+		/>
+	</svg>
+	<svg class="disc">
+		<circle
+			cx="50"
+			cy="50"
+			r={DISC_SIZE - DISC_OUTLINE}
+			fill={fillColour(slot)}
+			stroke={strokeColour(slot)}
+			stroke-width={DISC_OUTLINE}
+		/>
+	</svg>
+</div>
 
 <style>
 	.slot {
-		--slotImage: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Cpath d='M0 0v50A50 50 0 0 1 50 0H0zm50 0a50 50 0 0 1 50 50V0H50zm50 50a50 50 0 0 1-50 50h50V50zm-50 50A50 50 0 0 1 0 50v50h50z' style='fill:%2300a;fill-rule:evenodd;'/%3E%3Ccircle cx='50' cy='50' r='48' style='fill:none;stroke:%2300a;stroke-width:5;'/%3E%3C/svg%3E");
-		--discImage: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ccircle fill='%2388f' stroke='%23ddf' stroke-width='5' cx='50' cy='50' r='45' /%3E%3C/svg%3E%0A");
-		--discImagePrimary: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ccircle fill='%23e00' stroke='%23b00' stroke-width='5' cx='50' cy='50' r='45' /%3E%3C/svg%3E%0A");
-		--discImageSecondary: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ccircle fill='%23ffe819' stroke='%23ffc010' stroke-width='5' cx='50' cy='50' r='45' /%3E%3C/svg%3E%0A");
-		--discSize: 4rem;
-		display: grid;
-		place-content: center;
-		width: var(--discSize);
-		height: var(--discSize);
+		--c-board: #00e;
+		--c-board-outline: #00c;
+		--c-board-outline: #008;
+		--c-discHighlight: none;
+		--c-discHighlightOutline: none;
+		--c-discPrimary: #ffe819;
+		--c-discPrimaryOutline: #ffc010;
+		--c-discSecondary: #e00;
+		--c-discSecondaryOutline: #b00;
+		--slotSize: 5rem;
 		position: relative;
+		width: var(--slotSize);
+		height: var(--slotSize);
+	}
+	.slot svg {
+		position: relative;
+		inset: 0;
+		width: 100%;
+		height: 100%;
 		cursor: pointer;
 	}
-	.slot::after,
-	.slot::before {
-		content: '';
-		position: absolute;
-		inset: 0px;
-		background-repeat: no-repeat;
-		background-position: center;
-		background-image: none;
-	}
-	.slot::after {
-		content: '';
-		z-index: 1;
-		outline: 2px solid #00a;
-		outline-offset: -1px;
-		background-image: var(--slotImage);
-	}
-	.slot.hover::after {
-		z-index: 1;
-		filter: brightness(1.5);
-	}
-	.slot.drophint::before {
-		background-image: var(--discImage);
-	}
-	.slot.dance::before {
+	.disc {
 		isolation: isolate;
-		z-index: 2;
-		transform: scale(1.1);
-		animation: 250ms ease-out infinite alternate disc-dance;
-	}
-
-	.slot.primary::before {
-		background-image: var(--discImagePrimary);
-	}
-	.slot.secondary::before {
-		background-image: var(--discImageSecondary);
-	}
-
-	.slot:is(.primary, .secondary)::before {
+		z-index: 1;
 		filter: initial;
 		animation-name: disc-drop;
 		animation-duration: 1200ms;
 	}
+	/* .slot.dance::before { */
+	/* 	isolation: isolate; */
+	/* 	z-index: 2; */
+	/* 	transform: scale(1.2); */
+	/* 	animation: 250ms ease-out infinite alternate disc-dance; */
+	/* } */
 
 	@keyframes disc-dance {
 		0% {
